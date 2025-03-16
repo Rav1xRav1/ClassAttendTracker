2044cbfa41a92967ce0130b22b54cc1a8b83cccb
nishimuraippei <xicunyiping866@gmail.com>
2025-03-17 03:06:07 +0900

    holidays, schedule, gps_locationsテーブルを操作するクラスを追加

--- src/gps_tracker/psql.py
+++ src/gps_tracker/psql.py
@@ -1,37 +1,107 @@
 import psycopg2
 from psycopg2 import sql
-from datetime import datetime
+from datetime import datetime, time
 
+# holidaysテーブルのクラス
 class HOLIDAYS:
     def __init__(self, cursor):
         self.cursor = cursor
-
-    def fetch_all(self):
-        query = "SELECT * FROM holidays"
-        self.cursor.execute(query)
-        return self.cursor.fetchall()
     
     # 引数の日付が休暇が調べる
-    def is_holiday_date(self, date):
+    def is_holiday_date(self, today):
         query = """
         SELECT * FROM holidays
         WHERE start_date <= %s AND end_date >= %s
         AND (start_period IS NULL OR start_period <= %s)
         AND (end_period IS NULL OR end_period >= %s)
         """
-        self.cursor.execute(query, (date, date, 1, 1))
+        self.cursor.execute(query, (today.date(), today.date(), 1, 1))
         return self.cursor.fetchone()
     
     # 引数の日付が休暇だった場合、休暇終了までの秒数を取得する
-    def seconds_until_holiday_end(self, date):
-        holiday = self.is_holiday_date(date)
+    def seconds_until_holiday_end(self, today):
+        holiday = self.is_holiday_date(today)
         print(holiday)
         if holiday:
-            end_date = holiday[2]  # タプルのインデックスを使用
-            end_datetime = datetime.combine(end_date, datetime.min.time())
-            current_datetime = datetime.combine(date, datetime.min.time())
-            return (end_datetime - current_datetime).total_seconds()
+            end_date = holiday[2]
+            end_period = holiday[4]
+            if end_period is None:
+                end_datetime = datetime.combine(end_date, datetime.max.time())
+            else:
+                end_datetime = datetime.combine(end_date, time(end_period))
+            print(end_datetime, today)
+            return (end_datetime - today).total_seconds()
         return None
+
+# schedulesテーブルのクラス
+class SCHEDULES:
+    def __init__(self, cursor):
+        self.cursor = cursor
+
+    # 現在時刻から現在何限目かを取得する関数
+    def get_current_period(self, today_time):
+        print(today_time)
+        query = """
+        SELECT period FROM class_times
+        WHERE start_time <= %s AND end_time >= %s
+        LIMIT 1
+        """
+        self.cursor.execute(query, (today_time, today_time))
+        result = self.cursor.fetchone()
+        return result[0] if result else None
+
+    # 現在の曜日と時限数を使用して現在の授業を取得する関数
+    def get_current_class(self, today):
+        current_period = self.get_current_period(today.time())
+        if current_period is None:
+            return None
+        
+        query = """
+        SELECT * FROM schedules
+        WHERE weekday = %s AND period = %s
+        LIMIT 1
+        """
+        day_of_week = today.weekday()  # 月曜日が0、日曜日が6
+        self.cursor.execute(query, (day_of_week, current_period))
+        return self.cursor.fetchone()
+    
+    # 現在の時限数から授業開始までの秒数を取得する関数
+    def seconds_until_class_start(self, today_time):
+        current_period = self.get_current_period(today_time)
+        if current_period is None:
+            return None
+        
+        query = """
+        SELECT start_time FROM class_times
+        WHERE period = %s
+        """
+        self.cursor.execute(query, (current_period,))
+        start_time = self.cursor.fetchone()[0]
+        start_datetime = datetime.combine(datetime.today(), start_time)
+        return (start_datetime - datetime.today()).total_seconds()
+
+# 取得した位置情報を保存するテーブルを操作する
+class GPS_LOCATIONS:
+    def __init__(self, cursor, conn):
+        self.cursor = cursor
+        self.conn = conn  # conn属性を追加
+
+    # 全データを取得して返す
+    def fetch_all(self):
+        query = """
+        SELECT * FROM gps_locations
+        """
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    # 位置情報を保存する関数
+    def insert(self, lat, lon):
+        query = """
+        INSERT INTO gps_locations (latitude, longitude, timestamp)
+        VALUES (%s, %s, %s)
+        """
+        self.cursor.execute(query, (lat, lon, datetime.now()))
+        self.conn.commit()
 
 class PSQL:
     def __init__(self, DATABASE_URL, *tables):
@@ -44,7 +114,9 @@ class PSQL:
         self.cursor = self.conn.cursor()
         
         self.holidays = HOLIDAYS(self.cursor)
+        self.schedules = SCHEDULES(self.cursor)
+        self.gps_locations = GPS_LOCATIONS(self.cursor, self.conn)  # connを渡す
 
     def close(self):
         self.cursor.close()
-        self.connection.close()
+        self.conn.close()
