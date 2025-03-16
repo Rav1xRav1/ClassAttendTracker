9af6ea8bb05ecf9e6d8e5355f525d2400bb17ecc
nishimuraippei <xicunyiping866@gmail.com>
2025-03-17 03:05:33 +0900

    位置情報を取得して保存できるようにした

--- src/gps_tracker/main.py
+++ src/gps_tracker/main.py
@@ -7,19 +7,24 @@ import sys
 
 from time import sleep
 
+from psql import PSQL
+import logging
+
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
     user_name = os.environ.get('USER_NAME')
     password = os.environ.get('PASSWORD')
 
     api = PyiCloudService(user_name, password, cookie_directory="./.pyicloud", verify=True)
 
     # 2要素認証または2段階認証が必要かどうかを確認
-    print(api.requires_2fa, api.requires_2sa)
+    # print(api.requires_2fa, api.requires_2sa)
 
     # 2要素認証が必要な場合
     if api.requires_2fa:
-        print("Two-factor authentication required.")
+        logger.info("Two-factor authentication required.")
         # ユーザーに認証コードの入力を求める
         # code = input("Enter the code you received of one of your approved devices: ")
 
@@ -31,37 +36,37 @@ def main():
                     if code == "":
                         print("Code is empty.")
                     else:
-                        print("CODE: ", code)
+                        logger.info(f"CODE: {code}")
                         break
             except FileNotFoundError:
-                print("File not found.")
+                logger.error("File not found.")
                 sys.exit(1)
             
             sleep(30)
 
         # 認証コードを検証
         result = api.validate_2fa_code(code)
-        print(f"Code validation result: {result}")
+        logger.info(f"Code validation result: {result}")
 
         if not result:
-            print("Failed to verify security code")
+            logger.error("Failed to verify security code")
             sys.exit(1)
 
         # セッションが信頼されているかどうかを確認
         if not api.is_trusted_session:
-            print("Session is not trusted. Requesting trust...")
+            logger.info("Session is not trusted. Requesting trust...")
             # セッションの信頼をリクエスト
             result = api.trust_session()
-            print(f"Session trust result {result}")
+            logger.info(f"Session trust result {result}")
 
             if not result:
-                print("Failed to request trust. You will likely be prompted for the code again in the coming weeks")
+                logger.error("Failed to request trust. You will likely be prompted for the code again in the coming weeks")
 
     # 2段階認証が必要な場合
     elif api.requires_2sa:
         import click
 
-        print("Two-step authentication required. Your trusted devices are:")
+        logger.info("Two-step authentication required. Your trusted devices are:")
 
         # 信頼されたデバイスのリストを取得
         devices = api.trusted_devices
@@ -84,67 +89,69 @@ def main():
             print("Failed to verify verification code")
             sys.exit(1)
 
-    for i, device in enumerate(api.devices):
-        print(f"{i + 1}. {device}")
+    # for i, device in enumerate(api.devices):
+    #     print(f"{i + 1}. {device}")
 
-    today = datetime.datetime(2000, 1, 1)
+    psql = PSQL(os.getenv('DATABASE_URL'))
 
-    # データベースに接続する
-    DATABASE_URL= os.getenv('DATABASE_URL')
-    while True:
-        try:
-            conn = psycopg2.connect(DATABASE_URL)
-            break
-        except psycopg2.OperationalError:
-            print('Retrying connection...')
-
-    # カーソルを取得する
-    cursor = conn.cursor()
+    today = datetime.datetime.min
 
     # holidaysテーブルからすべてのデータを取り出して表示する
-    cursor.execute("SELECT * FROM holidays")
-    holidays = cursor.fetchall()
-    for holiday in holidays:
-        print(holiday)
-
-    # 現在のディレクトリを表示
-    current_directory = os.getcwd()
-    print(f"現在のディレクトリ: {current_directory}")
-
-    # 現在のディレクトリのファイルとフォルダをすべて表示
-    files_and_folders = os.listdir(current_directory)
-    print("ファイルとフォルダ:")
-    for item in files_and_folders:
-        print(item)
+    # holidays = psql.holidays.fetch_all()
+    # for holiday in holidays:
+    #     print(holiday)
 
     while True:
-        # 日付が変わったなら
-        if datetime.datetime.now() != today:
-            today = datetime.datetime.now()
+        today = datetime.datetime.today()
 
-            # 今日が授業除外日でないか確認する
-            # holidaysテーブルをクエリ
-            query = """
-            SELECT * FROM holidays
-            WHERE start_date <= %s AND end_date >= %s
-            AND (start_period IS NULL OR start_period <= %s)
-            AND (end_period IS NULL OR end_period >= %s)
-            """
-            t = datetime.datetime.today().date()
-            cursor.execute(query, (t, t, 1, 1))
-            result = cursor.fetchone()
+        # 今日が授業除外日でないか確認する
+        is_holiday = psql.holidays.is_holiday_date(datetime.datetime.today())
 
-            # 結果を確認
-            if result:
-                print("今日のコマは休暇に含まれています。")
+        # 結果を確認
+        if is_holiday:
+            logger.info("現在時刻は除外時刻です。")
+            # 休暇終了までの秒数を取得
+            seconds = psql.holidays.seconds_until_holiday_end(datetime.datetime.today())
+            logger.info(f"除外時間終了までスリープします。({seconds}秒)")
+            # for _ in range(86400): sleep(seconds / 86400)
+            # continue
+        
+        logger.info("今日のコマは休暇に含まれていません。")
+
+        # 現在時刻が授業時間に含まれているか確認
+        is_within_class_time = psql.schedules.get_current_period(today.time())
+        # 現在時刻が授業時間に含まれていない場合
+        if not is_within_class_time:
+            logger.info("現在時刻は授業時間外です。")
+            # 授業開始までの秒数を取得
+            seconds = psql.schedules.seconds_until_class_start(today.time())
+            # 本日これ以降授業があるなら
+            if seconds is not None:
+                logger.info(f"授業開始までスリープします。({seconds}秒)")
+                sleep(seconds)
+            # 本日これ以降授業がないなら
             else:
-                print("今日のコマは休暇に含まれていません。")
-            
-            break
+                # 次の日の午前0時までの秒数を取得
+                seconds = (datetime.datetime.combine(today, datetime.time.max) - today).total_seconds()
+                logger.info(f"明日までスリープします。({seconds}秒)")
+                # sleep(seconds)
+                # continue
+        # 現在時刻が授業時間内なら
+        else:
+            pass
 
-    lat_lon = api.devices[1].location()
+        lat_lon = api.iphone.location()
+        if lat_lon is None:
+            logger.error("Failed to get location.")
+            continue
 
-    print(lat_lon)
+        logger.info(lat_lon)
+
+        psql.gps_locations.insert(lat_lon['latitude'], lat_lon['longitude'])
+
+        print(psql.gps_locations.fetch_all())
+
+        break
 
 if __name__ == '__main__':
     main()
