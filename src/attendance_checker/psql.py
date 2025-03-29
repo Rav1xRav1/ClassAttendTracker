0687c752a63fe8cb93e007b8360d51f852fc6588
nishimuraippei <xicunyiping866@gmail.com>
2025-03-30 01:38:53 +0900

    first commit

---
+++ src/attendance_checker/psql.py
@@ -0,0 +1,55 @@
+import psycopg2
+from psycopg2 import sql
+from datetime import datetime, time
+
+
+class CLASS_LOCATIONS:
+    def __init__(self, cursor):
+        self.cursor = cursor
+
+    # 全ての情報を返す
+    def fetch_all(self):
+        query = """
+        SELECT * FROM locations
+        """
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+    
+    def get_coordinates(self, location_id):
+        query = """
+        SELECT latitude, longitude FROM locations WHERE id = %s
+        """
+        self.cursor.execute(query, (location_id,))
+        result = self.cursor.fetchone()
+        return result if result else None
+
+
+class SCHEDULES:
+    def __init__(self, cursor):
+        self.cursor = cursor
+
+    #  引数の曜日の授業を取得する関数
+    def get_classes_by_weekday(self, weekday):
+        query = """
+        SELECT * FROM schedules WHERE weekday = %s
+        """
+        self.cursor.execute(query, (weekday,))
+        return self.cursor.fetchall()
+
+
+class PSQL:
+    def __init__(self, DATABASE_URL, *tables):
+        while True:
+            try:
+                self.conn = psycopg2.connect(DATABASE_URL)
+                break
+            except psycopg2.OperationalError:
+                print('Retrying connection...')
+        self.cursor = self.conn.cursor()
+        
+        self.class_locations = CLASS_LOCATIONS(self.cursor)
+        self.schedules = SCHEDULES(self.cursor)
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
